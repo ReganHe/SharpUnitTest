9a9043c4ad6c21ad0f371a7b817a735185e3c79c
何柯 <heke@hujiang.com>
2016-01-11 18:54:10 +0800

    删除多余代码

--- MyUnitTest/Code/HK.UnitTestDemo/HK.SelfMock.UnitTests/LogAnalyzerTest.cs
+++ MyUnitTest/Code/HK.UnitTestDemo/HK.SelfMock.UnitTests/LogAnalyzerTest.cs
@@ -8,16 +8,6 @@ namespace HK.SelfMock.UnitTests
     public class LogAnalyzerTest
     {
         [Fact]
-        public void Analyzer_TooShortFileName_CallsWebService()
-        {
-            var mockService = new FakeWebService();
-            var log = new LogAnalyzer(mockService);
-            const string tooShortFileName = "abc.ext";
-            log.Analyze(tooShortFileName);
-            Assert.Equal("FileName too short:abc.ext", mockService.LastError);
-        }
-
-        [Fact]
         public void Analyze_WebServiceThrows_ThrowException()
         {
             var stubService = new FakeWebService
@@ -28,6 +18,16 @@ namespace HK.SelfMock.UnitTests
             var log = new LogAnalyzer(stubService);
             const string tooShortFileName = "abc.ext";
             Assert.Throws<Exception>(() => log.Analyze(tooShortFileName));
+        }
+
+        [Fact]
+        public void Analyzer_TooShortFileName_CallsWebService()
+        {
+            var mockService = new FakeWebService();
+            var log = new LogAnalyzer(mockService);
+            const string tooShortFileName = "abc.ext";
+            log.Analyze(tooShortFileName);
+            Assert.Equal("FileName too short:abc.ext", mockService.LastError);
         }
     }
 }
